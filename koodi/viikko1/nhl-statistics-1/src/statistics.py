e73af0dec5b9f432fc162d48728547c96a427bf8
Shmuli02 <samuli.kaleva@gmail.com>
2021-10-29 09:04:01 +0000

    riippuvuuksien injektointi

--- koodi/viikko1/nhl-statistics-1/src/statistics.py
+++ koodi/viikko1/nhl-statistics-1/src/statistics.py
@@ -6,10 +6,8 @@ def sort_by_points(player):
 
 
 class Statistics:
-    def __init__(self):
-        reader = PlayerReader(
-            "https://nhlstatisticsforohtu.herokuapp.com/players.txt"
-        )
+    def __init__(self,playerReader):
+        reader = playerReader
 
         self._players = reader.get_players()
 
