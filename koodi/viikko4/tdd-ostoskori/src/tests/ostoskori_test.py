fb8121cea67df0b8579e2dff96373734c1487cf2
Shmuli02 <samuli.kaleva@gmail.com>
2021-11-29 08:16:59 +0000

    vk4 teht.5.6

--- koodi/viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ koodi/viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -30,3 +30,9 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(self.juusto)
         
         self.assertEqual(self.kori.hinta(), 8)
+    
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_ostoskorissa_on_kaksi_tavataa(self):
+        self.kori.lisaa_tuote(self.juusto)
+        self.kori.lisaa_tuote(self.juusto)
+
+        self.assertEqual(self.kori.tavaroita_korissa(),2)
