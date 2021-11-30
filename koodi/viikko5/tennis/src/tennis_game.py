76696a9c079521b4483ac28f67a716117b94f48b
Shmuli02 <samuli.kaleva@gmail.com>
2021-11-30 09:05:26 +0000

    vk5 teht.3

--- koodi/viikko5/tennis/src/tennis_game.py
+++ koodi/viikko5/tennis/src/tennis_game.py
@@ -2,56 +2,51 @@ class TennisGame:
     def __init__(self, player1_name, player2_name):
         self.player1_name = player1_name
         self.player2_name = player2_name
-        self.m_score1 = 0
-        self.m_score2 = 0
+        self.player1_score = 0
+        self.player2_score = 0
+        self.score_text = {
+            '00':'Love-All',
+            '11':'Fifteen-All',
+            '22':'Thirty-All',
+            '33':'Forty-All',
+            '0':'Love',
+            '1':'Fifteen',
+            '2':'Thirty',
+            '3':'Forty',
+            'D':'Deuce',
+            'A1':f"Advantage {self.player1_name}",
+            'A2':f"Advantage {self.player2_name}",
+            'W1':f"Win for {self.player1_name}",
+            'W2':f"Win for {self.player2_name}"
+        }
 
     def won_point(self, player_name):
-        if player_name == "player1":
-            self.m_score1 = self.m_score1 + 1
+        if player_name == self.player1_name:
+            self.player1_score += 1
         else:
-            self.m_score2 = self.m_score2 + 1
+            self.player2_score += 1
 
     def get_score(self):
         score = ""
-        temp_score = 0
 
-        if self.m_score1 == self.m_score2:
-            if self.m_score1 == 0:
-                score = "Love-All"
-            elif self.m_score1 == 1:
-                score = "Fifteen-All"
-            elif self.m_score1 == 2:
-                score = "Thirty-All"
-            elif self.m_score1 == 3:
-                score = "Forty-All"
+        if self.player1_score == self.player2_score:
+            if self.player2_score <= 3:
+                score = self.score_text[str(self.player1_score)+str(self.player2_score)]
             else:
-                score = "Deuce"
-        elif self.m_score1 >= 4 or self.m_score2 >= 4:
-            minus_result = self.m_score1 - self. m_score2
+                score = self.score_text['D']
 
-            if minus_result == 1:
-                score = "Advantage player1"
-            elif minus_result == -1:
-                score = "Advantage player2"
-            elif minus_result >= 2:
-                score = "Win for player1"
+        elif self.player1_score >= 4 or self.player2_score >= 4:
+            point_diff = self.player1_score - self.player2_score
+
+            if point_diff == 1:
+                score = self.score_text['A1']
+            elif point_diff == -1:
+                score = self.score_text['A2']
+            elif point_diff >= 2:
+                score = self.score_text['W1']
             else:
-                score = "Win for player2"
+                score = self.score_text['W2']
         else:
-            for i in range(1, 3):
-                if i == 1:
-                    temp_score = self.m_score1
-                else:
-                    score = score + "-"
-                    temp_score = self.m_score2
-
-                if temp_score == 0:
-                    score = score + "Love"
-                elif temp_score == 1:
-                    score = score + "Fifteen"
-                elif temp_score == 2:
-                    score = score + "Thirty"
-                elif temp_score == 3:
-                    score = score + "Forty"
+            return f"{self.score_text[str(self.player1_score)]}-{self.score_text[str(self.player2_score)]}"
 
         return score
