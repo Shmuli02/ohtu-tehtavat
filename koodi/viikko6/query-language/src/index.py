92487a4ebdf059cbcfca406b7b17bd6728d7c4b8
Shmuli02 <samuli.kaleva@gmail.com>
2021-12-12 10:46:15 +0000

    vk6 teht.5

--- koodi/viikko6/query-language/src/index.py
+++ koodi/viikko6/query-language/src/index.py
@@ -1,19 +1,25 @@
 from statistics import Statistics
 from player_reader import PlayerReader
 from matchers import And, HasAtLeast, PlaysIn, Not, All, HasFewerThan, Or, QueryBuilder
-
 def main():
     url = "https://nhlstatisticsforohtu.herokuapp.com/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
-    # matcher = And(
-    #     HasAtLeast(5, "goals"),
-    #     HasAtLeast(5, "assists"),
-    #     PlaysIn("PHI")
-    # )
     query = QueryBuilder()
-    matcher = query.playsIn("NYR").hasAtLeast(5, "goals").hasFewerThan(10, "goals").build()
+    matcher = (
+    query
+        .oneOf(
+        query.playsIn("PHI")
+            .hasAtLeast(10, "assists")
+            .hasFewerThan(5, "goals")
+            .build(),
+        query.playsIn("EDM")
+            .hasAtLeast(40, "points")
+            .build()
+        )
+        .build()
+    )
 
     for player in stats.matches(matcher):
         print(player)
